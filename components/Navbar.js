f11b4a6a35eb027451c21fcc4b146a9806d3d8b8
Vishu Bansal <bansalvishu56@gmail.com>
2024-01-26 23:55:08 +0530

    Done

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex py-3 flex-wrap justify-around">
+      <h1 className="text-4xl font-bold text-orange-600"> Kaam Karle</h1>
+    </div>
+  );
+};
+
+export default Navbar;
